cec05a90a7571b67ffb96d2b72901f16ed65a8c0
ilhamjayakusumah <ilhamjayakusumah24@gmail.com>
2021-09-15 14:20:12 +0700

    Create weekone.php

---
+++ weekone.php
@@ -0,0 +1,25 @@
+<?php 
+    $namalengkap="Ilham Jaya Kusumah";
+    $namapanggilan = "Hamdun";
+    $alamat = "Kawali";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>First Week Meet-Ilham Jaya Kusumah </title>
+</head>
+<body>
+        <?php
+        echo "------------------------------------------------- <br>";
+        echo "My Biodata <br>";
+        echo "------------------------------------------------- <br>";
+        echo "Nama Lengkap   : ".$namalengkap;
+        echo "<br>";
+        echo "Nama Panggilan : ". $namapanggilan;
+        echo "<br>";
+        echo "Domisili : ". $alamat;
+        echo "<br>";
+        echo "-------------------------------------------------";
+        ?>
+</body>
+</html>
