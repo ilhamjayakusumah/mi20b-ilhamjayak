bc633d685c8f23242448f05a51e22b9cb50472c1
ilhamjayakusumah <ilhamjayakusumah24@gmail.com>
2021-09-22 10:20:37 +0700

    Create array.php

---
+++ array.php
@@ -0,0 +1,21 @@
+<?php 
+ 
+$mybiodata = array(
+    "Nama : ilham Jaya",
+    "Alamat : Kawali",
+    "email : kusumah@gmail.com",
+    "No HP : 085711372583"
+);
+// pemanggilan array 
+echo $mybiodata[2];
+ 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
